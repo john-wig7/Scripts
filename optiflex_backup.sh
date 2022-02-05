581bae3da50e692b84277ed9642116ab180db92b
john-wig7 <john@kohukohu.com>
2022-02-05 13:57:45 +1300

    Updated both of these scripts with small modifications

--- optiflex_backup.sh
+++ optiflex_backup.sh
@@ -2,8 +2,15 @@
 
 # A script to backup my optiflex My Documents folder to the drive attached to this pi
 
+# The source files are mounted in this location - it depends on Optiflex computer to be running and correctly mounted
+# I have not yet tested any errors if this is not the case
+THESOURCE="/home/pi/optiflexDocs/"
+
+# The destination is the Toshiba 2 TB Drive USB DRive automounted by linux with this crazy number
+THEDESTINATION="/media/pi/5E4A83B74A838A8B/OptiflexBackup"
+
 TIMESTAMP=`date "+%d/%m/%Y %H:%M:%S"`
 LOGFILE='/home/pi/Scripts/backuplog'
 msgSuccess="$TIMESTAMP: Successful Optiflex MyDocuments Backup."
 
-rsync -avr  /home/pi/optiflexDocs/ /media/pi/5E4A83B74A838A8B/OptiflexBackup && echo $msgSuccess >> $LOGFILE 
+rsync -avr  $THESOURCE $THEDESTINATION && echo $msgSuccess >> $LOGFILE
--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# This is a sandpit for trying new ideas and learning how BASH works
+
+# variables
 animal='crocodile'
+echo $animal
+
+#assigning function to variable with backtick
+todaysdate=`date "+%d/%m/%Y %H:%M:%S"`
+echo $todaysdate
