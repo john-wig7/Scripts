54b69c711099a1dde09224e1138c4f5d2a9ea657
john-wig7 <john@kohukohu.com>
2022-02-05 17:45:20 +1300

    update my test script with new examples

--- test.sh
+++ test.sh
@@ -5,7 +5,37 @@
 # variables
 animal='crocodile'
 echo $animal
+echo "${animal}"
+
 
 #assigning function to variable with backtick
 todaysdate=`date "+%d/%m/%Y %H:%M:%S"`
 echo $todaysdate
+echo "${todaysdate}"
+
+myArray=("one" "two" "three")
+echo "${myArray[0]}"
+echo "${myArray[1]}"
+echo "${myArray[2]}"
+
+a=1234
+b=12.34
+c="some text"
+
+echo "${a}"
+echo "${b}"
+echo "${c}"
+
+#functions
+
+function myfunc () {
+    echo "this is the result" 
+    echo "for all to see"
+    echo "again and again"
+    b=12.35
+    echo $b
+}
+
+result=`myfunc`
+echo $result
+echo $b
