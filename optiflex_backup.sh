e90658869a782c21ce0933ce3c969a2b27fed428
john-wig7 <john@kohukohu.com>
2022-02-05 17:59:04 +1300

    more markup for each command amd variable defined

--- optiflex_backup.sh
+++ optiflex_backup.sh
@@ -9,12 +9,12 @@ THESOURCE="/home/pi/optiflexDocs/"
 # The destination is the Toshiba 2 TB Drive USB Drive automounted by linux with this crazy number
 THEDESTINATION="/media/pi/5E4A83B74A838A8B/OptiflexBackup"
 
-TIMESTAMP=`date "+%d/%m/%Y %H:%M:%S"`
-LOGFILE='/home/pi/Scripts/backuplog'
-ERRORLOG='/home/pi/Scripts/backuperror.log'
-OUTPUTLOG='/home/pi/Scripts/backupout.log'
-msgSuccess="$TIMESTAMP: Successful Optiflex MyDocuments Backup."
+TIMESTAMP=`date "+%d/%m/%Y %H:%M:%S"`   #current date and time
+LOGFILE='/home/pi/Scripts/backuplog'    #backuplog contains list of each backup including timestamp and whether the backup was successful or not  
+ERRORLOG='/home/pi/Scripts/backuperror.log'   #if the backup has an error - the error output will go to this log file
+OUTPUTLOG='/home/pi/Scripts/backupout.log'      #if the backup is successful - the list of backed up files will go to this log file
+msgSuccess="$TIMESTAMP: Successful Optiflex MyDocuments Backup."     
 msgError="$TIMESTAMP: There was an error in the backup process!"
 
-
-rsync -avr  $THESOURCE $THEDESTINATION 2>$ERRORLOG 1>$OUTPUTLOG && echo $msgSuccess >> $LOGFILE || echo $msgError >> $LOGFILE
+# This is the main rsync command - a for archive - v for vebose and r for recursive
+rsync -avr $THESOURCE $THEDESTINATION 2>$ERRORLOG 1>$OUTPUTLOG && echo $msgSuccess >> $LOGFILE || echo $msgError >> $LOGFILE
