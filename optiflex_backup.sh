ac6a621fbfd168c7a2639d2c92eb2b04c2638bff
john-wig7 <john@kohukohu.com>
2022-02-06 10:27:27 +1300

    moved logs to backuplog folder and edited script

--- optiflex_backup.sh
+++ optiflex_backup.sh
@@ -10,11 +10,11 @@ THESOURCE="/home/pi/optiflexDocs/"
 THEDESTINATION="/media/pi/5E4A83B74A838A8B/OptiflexBackup"
 
 TIMESTAMP=`date "+%d/%m/%Y %H:%M:%S"`   #current date and time
-LOGFILE='/home/pi/Scripts/backuplog'    #backuplog contains list of each backup including timestamp and whether the backup was successful or not  
-ERRORLOG='/home/pi/Scripts/backuperror.log'   #if the backup has an error - the error output will go to this log file
-OUTPUTLOG='/home/pi/Scripts/backupout.log'      #if the backup is successful - the list of backed up files will go to this log file
+LOGFILE='/home/pi/backupLogs/backuplog'    #backuplog contains list of each backup including timestamp and whether the backup was successful or not  
+ERRORLOG='/home/pi/backupLogs/backuperror.log'   #if the backup has an error - the error output will go to this log file
+OUTPUTLOG='/home/pi/backupLogs/backupout.log'      #if the backup is successful - the list of backed up files will go to this log file
 msgSuccess="$TIMESTAMP: Successful Optiflex MyDocuments backup."     
-msgError="$TIMESTAMP: There was an error in the backup process! Read the error log - backuperror.log"
+msgError="$TIMESTAMP: There was an error in the backup process! Read the error log - /home/pi/backupLogs/backuperror.log"
 
 # This is the main rsync command of this script, options are - a for archive and v for vebose 
 rsync -av $THESOURCE $THEDESTINATION 2>$ERRORLOG 1>$OUTPUTLOG && echo $msgSuccess >> $LOGFILE || echo $msgError >> $LOGFILE
