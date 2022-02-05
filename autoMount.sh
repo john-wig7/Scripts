0e99a739b31018b31878a340dde668ba6c34e509
john-wig7 <john@kohukohu.com>
2022-02-05 13:41:00 +1300

    Updated the script note

--- autoMount.sh
+++ autoMount.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# This is used by Crontab to auto mount my windows optiflex share.
-# It doesn't seem to want to automatically mount without this.
+# This is used by Crontab to auto mount my windows optiflex share after a reboot.
+# It doesn't automatically mount the share, so I send this command which mounts the share 3 seconds after reboot.
+# Crontab command is: @reboot sleep 3 && /home/pi/Scripts/autoMount.sh
+
 
 sudo mount  -a
